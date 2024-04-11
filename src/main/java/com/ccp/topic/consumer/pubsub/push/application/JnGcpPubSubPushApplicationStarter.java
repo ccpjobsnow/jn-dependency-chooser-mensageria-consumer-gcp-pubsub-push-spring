50ce95039e3345c1e2f7f057becde0c6bed13734
J4550N51LV4 <jasson@noxxonsat.com.br>
2024-04-11 20:07:04 -0300

    Verificando2

--- src/main/java/com/ccp/topic/consumer/pubsub/push/application/JnGcpPubSubPushApplicationStarter.java
+++ src/main/java/com/ccp/topic/consumer/pubsub/push/application/JnGcpPubSubPushApplicationStarter.java
@@ -1,5 +1,10 @@
 package com.ccp.topic.consumer.pubsub.push.application;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 import com.ccp.implementations.db.bulk.elasticsearch.CcpElasticSerchDbBulk;
 import com.ccp.implementations.db.dao.elasticsearch.CcpElasticSearchDao;
 import com.ccp.implementations.db.query.elasticsearch.CcpElasticSearchQueryExecutor;
@@ -10,8 +15,16 @@ import com.ccp.implementations.http.apache.mime.CcpApacheMimeHttp;
 import com.ccp.implementations.instant.messenger.telegram.CcpTelegramInstantMessenger;
 import com.ccp.implementations.json.gson.CcpGsonJsonHandler;
 import com.ccp.jn.async.business.JnAsyncBusinessNotifyError;
+import com.ccp.topic.consumer.pubsub.push.controller.CcpGcpPubSubConsumerController;
+import com.ccp.topic.consumer.pubsub.push.exception.handler.CcpGcpPubSubPushExceptionHandler;
 import com.jn.commons.entities.JnEntityAsyncTask;
 
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@ComponentScan(basePackageClasses = {
+		CcpGcpPubSubConsumerController.class,
+		CcpGcpPubSubPushExceptionHandler.class
+})
+@SpringBootApplication
 public class JnGcpPubSubPushApplicationStarter {
 	public static void main(String[] args) {
 		CcpGcpPubSubPushApplicationStarter.start(
