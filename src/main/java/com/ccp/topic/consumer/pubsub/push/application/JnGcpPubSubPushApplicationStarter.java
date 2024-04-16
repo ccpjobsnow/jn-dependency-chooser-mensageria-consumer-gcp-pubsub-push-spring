c3c0158ee10f5e4d4c8702ad83c9d459f1c3e699
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-16 18:26:59 -0300

    .

--- src/main/java/com/ccp/topic/consumer/pubsub/push/application/JnGcpPubSubPushApplicationStarter.java
+++ src/main/java/com/ccp/topic/consumer/pubsub/push/application/JnGcpPubSubPushApplicationStarter.java
@@ -1,10 +1,13 @@
 package com.ccp.topic.consumer.pubsub.push.application;
 
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.ccp.dependency.injection.CcpDependencyInjection;
 import com.ccp.implementations.db.bulk.elasticsearch.CcpElasticSerchDbBulk;
 import com.ccp.implementations.db.dao.elasticsearch.CcpElasticSearchDao;
 import com.ccp.implementations.db.query.elasticsearch.CcpElasticSearchQueryExecutor;
@@ -14,24 +17,18 @@ import com.ccp.implementations.file.bucket.gcp.CcpGcpFileBucket;
 import com.ccp.implementations.http.apache.mime.CcpApacheMimeHttp;
 import com.ccp.implementations.instant.messenger.telegram.CcpTelegramInstantMessenger;
 import com.ccp.implementations.json.gson.CcpGsonJsonHandler;
-import com.ccp.jn.async.business.JnAsyncBusinessNotifyError;
-import com.ccp.topic.consumer.pubsub.push.controller.CcpGcpPubSubConsumerController;
-import com.ccp.topic.consumer.pubsub.push.exception.handler.CcpGcpPubSubPushExceptionHandler;
-import com.jn.commons.entities.JnEntityAsyncTask;
 
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 @ComponentScan(basePackageClasses = {
-		CcpGcpPubSubConsumerController.class,
-		CcpGcpPubSubPushExceptionHandler.class
+		JnGcpPubSubConsumerController.class,
+		JnGcpPubSubPushExceptionHandler.class
 })
 @SpringBootApplication
 public class JnGcpPubSubPushApplicationStarter {
+
 	public static void main(String[] args) {
-		CcpGcpPubSubPushApplicationStarter.start(
-				new JnAsyncBusinessNotifyError(), 
-				new JnEntityAsyncTask(), 
-				args,
-				new CcpGcpFileBucket(),
+		CcpDependencyInjection.loadAllDependencies( 
+				new CcpGcpFileBucket(),  
 				new CcpApacheMimeHttp(),
 				new CcpGsonJsonHandler(),
 				new CcpElasticSearchDao(),
@@ -41,7 +38,10 @@ public class JnGcpPubSubPushApplicationStarter {
 				new CcpTelegramInstantMessenger(),
 				new CcpElasticSearchQueryExecutor()
 				);
-	}
+		SpringApplication.run(JnGcpPubSubPushApplicationStarter.class, args);
 
+		
+		
+	}
 	
 }
